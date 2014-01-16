84725fffe5670f1b48567b009084d31074feeacd
Leonardo Souza <lsouzaassis@gmail.com>
2014-01-16 14:52:05 -0300

    Arrumado erro da exclusão do restaurante

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -11,4 +11,5 @@ class Prato < ActiveRecord::Base
 	private
 	def validate_presence_of_more_than_one_restaurante
 		errors.add("restaurantes", "deve haver ao menos um restaurante") if restaurantes.empty
+	end
 end
